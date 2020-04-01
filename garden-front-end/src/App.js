d7cafe1f00db89757f821c68651516ef477ddbf7
Joshua Reed <joshualreed93@gmail.com>
2020-04-01 09:27:02 -0400

    initial page files created

--- garden-front-end/src/App.js
+++ garden-front-end/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Router, Route } from "react-router";
+import "./App.css";
 
 function App() {
   return (
-    
+    <Router>
+      <Route path={} />
+      <Route path={} />
+    </Router>
   );
 }
 
