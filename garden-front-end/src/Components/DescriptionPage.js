195399e0bad556b14c878f57169226d8281514cd
Joshua Reed <joshualreed93@gmail.com>
2020-04-09 13:59:20 -0400

    fix home picture

--- garden-front-end/src/Components/DescriptionPage.js
+++ garden-front-end/src/Components/DescriptionPage.js
@@ -4,36 +4,18 @@ class DescriptionPage extends Component {
   constructor() {
     super();
     this.state = {
-      data: {}
+      data: []
     };
   }
-  componentWillMount() {
-    const { name } = this.props.match.params;
-
-    let BaseUrl = "https://garden-api-1993.herokuapp.com/vegetables/name/";
-    fetch(BaseUrl + name)
+  componentDidMount() {
+    let BaseUrl = "https://garden-api-1993.herokuapp.com/vegetables";
+    fetch(BaseUrl)
       .then(res => {
         return res.json();
       })
       .then(res => {
         console.log(res);
-        this.setState({ data: res[0] });
-      })
-      .then(res => {
-        console.log(this.state.data);
-      });
-  }
-  componentDidUpdate() {
-    const { name } = this.props.match.params;
-
-    let BaseUrl = "https://garden-api-1993.herokuapp.com/vegetables/name/";
-    fetch(BaseUrl + name)
-      .then(res => {
-        return res.json();
-      })
-      .then(res => {
-        console.log(res);
-        this.setState({ data: res[0] });
+        this.setState({ data: res });
       })
       .then(res => {
         console.log(this.state.data);
@@ -41,35 +23,44 @@ class DescriptionPage extends Component {
   }
 
   render() {
-    let veg = this.state.data;
+    const { name } = this.props.match.params;
+    let result = this.state.data;
 
-    return (
-      <div>
-        <div className="infoContainer">
-          <div
-            className="DescriptionImage"
-            style={{
-              backgroundImage: `url(${veg.images})`
-            }}
-          ></div>
-          <ul className="info">
-            <li className="listItem" style={{ fontSize: "45px" }}>
-              {veg.name}
-            </li>
-            <li className="listItem">{veg.culinaryHints}</li>
-          </ul>
-        </div>
-        <div>
-          <ul className="instructions">
-            <li className="instructionList">{veg.sowInstructions}</li>
-            <li className="instructionList">{veg.spaceInstructions}</li>
-            <li className="instructionList">{veg.harvestInstructions}</li>
-            <li className="instructionList">{veg.compatiblePlants}</li>
-            <li className="instructionList">{veg.avoidInstructions}</li>
-          </ul>
-        </div>
-      </div>
-    );
+    let veg = result.map(item => {
+      if (item.name == name) {
+        console.log(item);
+        return (
+          <div>
+            <div className="infoContainer">
+              <div
+                className="DescriptionImage"
+                style={{
+                  backgroundImage: `url(${item.images})`
+                }}
+              ></div>
+              <ul className="info">
+                <li className="listItem" style={{ fontSize: "45px" }}>
+                  {item.name}
+                </li>
+                <li className="listItem">{item.culinaryHints}</li>
+              </ul>
+            </div>
+            <div>
+              <ul className="instructions">
+                <li className="instructionList">{item.sowInstructions}</li>
+                <li className="instructionList">{item.spaceInstructions}</li>
+                <li className="instructionList">{item.harvestInstructions}</li>
+                <li className="instructionList">{item.compatiblePlants}</li>
+                <li className="instructionList">{item.avoidInstructions}</li>
+              </ul>
+            </div>
+          </div>
+        );
+      } else {
+      }
+    });
+
+    return <div>{veg}</div>;
   }
 }
 
